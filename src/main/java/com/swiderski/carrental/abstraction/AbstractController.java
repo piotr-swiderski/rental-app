7a38f90870cf2cf0088fe23ad268eeff4e33afb2
Piotr Swiderski <piotr.swiderski@ttsw.com.pl>
2020-07-16 15:34:43 +0200

    Added drop db migration

--- src/main/java/com/swiderski/carrental/abstraction/AbstractController.java
+++ src/main/java/com/swiderski/carrental/abstraction/AbstractController.java
@@ -2,7 +2,14 @@ package com.swiderski.carrental.abstraction;
 
 import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin
 public abstract class AbstractController<T extends CommonService<E>, E extends AbstractDto> {
---
+++ src/main/resources/db/migration/V0__drop_db.sql
@@ -0,0 +1,8 @@
+drop table if EXISTS rental_table;
+drop table if EXISTS car_table;
+drop table if EXISTS client_table;
+drop table if EXISTS permission_role;
+drop table if exists role_user;
+drop table if EXISTS user_table;
+drop table if EXISTS role;
+drop table if EXISTS permission;
